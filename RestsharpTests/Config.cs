daf3a851bf30fe0fca7cb53b402903f4f557d057
Tatyana Durova <durova@octonus.com>
2016-08-11 19:06:19 +0300

    Add implicitClent class

--- RestsharpTests/Config.cs
+++ RestsharpTests/Config.cs
@@ -4,14 +4,19 @@ namespace RestsharpTests
 {
     public class Config
     {
+        public static string loginPageUrl;
         public static string ApplicationMainUrl => ConfigurationManager.AppSettings["applicationMainUrl"];
 
         public static string AppLogin => ConfigurationManager.AppSettings["login"];
 
         public static string AppPassword => ConfigurationManager.AppSettings["password"];
 
-        public static object ConsumerId { get; internal set; }
+        public static string ConsumerId { get; internal set; }
 
-        public static object RedirectUrl { get; internal set; }
+        public static string RedirectUrl { get; internal set; }
+
+        public static string LoginPageUrl { get; internal set; }
+
+        
     }
 }
