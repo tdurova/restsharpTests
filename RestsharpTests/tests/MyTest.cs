ed7aefeeda088eb243c06edb13fa4886b6c72fda
Tatyana Durova <durova@octonus.com>
2016-08-18 11:58:48 +0300

    try to use Nlog

--- RestsharpTests/tests/MyTest.cs
+++ RestsharpTests/tests/MyTest.cs
@@ -26,8 +26,18 @@ namespace RestsharpTests.tests
             var token = helperBase.GetAuthToken(Config.AppLogin, Config.AppPassword);
 
             Console.WriteLine(token.access_token);
-            
+
             RestsharpClient.Client.Authenticator = new OAuth2UriQueryParameterAuthenticator(token.access_token); //works
+            var request = new RestRequest("api/frontend/v1/profiles/5", Method.GET);
+
+            RestsharpClient.Client.Authenticator.Authenticate(RestsharpClient.Client, request);
+
+            // execute the request
+            IRestResponse response = RestsharpClient.Client.Execute(request);
+            var content = response.Content; // raw content as string
+            Console.WriteLine(content);
+
+          
         }
     }
 }
